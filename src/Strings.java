d3893848376b505efb80f8757ed885c6df49ff18
Xakerzz <85760425+Xakerzz@users.noreply.github.com>
2021-09-07 00:00:03 +0300

    origin

---
+++ src/Strings.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Strings {
+    public static void main(String[] args) throws FileNotFoundException {
+        String str1 = "Hello World 123";
+        String str2 = new String("abc");
+        String str3 = "abc";
+        str2=str2.intern();
+//        System.out.println(str1 == str2);
+
+        Scanner scanner = new Scanner(new File("C:\\Users\\xaker\\Skillfactory\\Module2\\src\\test.txt"));
+
+        while (!scanner.hasNextInt()){
+            System.out.println(scanner.next());
+        }
+        System.out.println(scanner.nextInt()%5);
+
+    }
+}
--- src/Vebinar.java
+++ src/Vebinar.java
@@ -1,17 +1,55 @@
+import java.sql.Array;
+import java.util.Arrays;
+
 public class Vebinar {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 10;
-        if (a > b){
-            System.out.println("a is greater than b");
-        } else if (a == b){
-            System.out.println("a equals b");
-        }
-        else {
-            System.out.println("b is greater than a");
-        }
+//    public static void main(String[] args) {
+//        int a = 10;
+//        int b = 10;
+//        if (a > b){
+//            System.out.println("a is greater than b");
+//        } else if (a == b){
+//            System.out.println("a equals b");
+//        }
+//        else {
+//            System.out.println("b is greater than a");
+//        }
+//    }
+//    int c = 0;
+//    int d = 0;
+//    int  h =0;
+public static void main(String[] args) {
+    int arr[];
+    int [] arr1= {1,2,3,4,5};
+    int [] arr2= new int[20];
+    int [] arr3 = new int[]{1,2,3,4,5};
+    System.out.println(Arrays.toString(arr1));
+    System.out.println(Arrays.toString(arr2));
+    System.out.println(Arrays.toString(arr3));
+    int a = 20;
+    int b =35;
+//    while (b > a){
+//        System.out.println("1: " + (b-a));
+//        a++;
+//    }
+//
+//    do {
+//        System.out.println("2: " + (b-a));
+//        a++;
+//    }while (b>a);
+
+    for (int i = 0; i <arr1.length ; i++) {
+        System.out.println(i + ": " + arr1[i]);
     }
-    int c = 0;
-    int d = 0;
-    int  h =0;
+
+    for (int item:arr1) {
+        if (item==3){
+            continue;
+        }
+        System.out.println(item);
+    }
+}
+
+
+
+
 }
